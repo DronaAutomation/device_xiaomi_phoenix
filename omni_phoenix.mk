2bd9065b84f2990c36de1f5742ec57d6e543c629
Drona <43744806+shivamjadon@users.noreply.github.com>
2020-12-13 06:14:20 +0530

    Update and rename aosip_phoenix.mk to omni_phoenix.mk

--- omni_phoenix.mk
+++ omni_phoenix.mk
@@ -7,10 +7,10 @@
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
 # Inherit some common AOSiP stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosip_phoenix
+PRODUCT_NAME := omni_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
